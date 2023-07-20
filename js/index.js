9543ce5078de1edc0a2784292a9fb30d6f34f87d
Muhammad Hussaini <hussainimuhammad999@gmail.com>
2023-07-20 10:09:16 +0530

    Added a simple taskbar and search

--- js/index.js
+++ js/index.js
@@ -1,4 +1,35 @@
+let commandTextInput = document.querySelector("#command")
+let searchFilter = document.querySelector(".search-filter")
+let programList = document.querySelectorAll(".command")
+
+document.addEventListener("click", () => {
+    commandSearch()
+})
+
 document.addEventListener("keyup", (e) => {
-    if (e.key == '/') document.querySelector("#command").focus()
-    if (e.key == 'Escape') document.querySelector("#command").blur()
+    if (e.key == '/') {
+        commandTextInput.focus()
+        commandSearch()
+    }
+    if (e.key == 'Escape') { commandTextInput.blur(); commandSearch() }
+
+})
+
+function commandSearch() {
+    if (document.activeElement == commandTextInput) {
+        searchFilter.style.display = 'flex'
+    }
+    else {
+        searchFilter.style.display = 'none'
+    }
+}
+
+commandTextInput.addEventListener("keyup", (e) => {
+    let input = commandTextInput.value
+    programList.forEach(element => {
+        let name = element.childNodes[3].childNodes[1]
+        if (name.innerHTML.startsWith(input)) {
+            element.style.display = 'flex'
+        } else element.style.display = 'none'
+    });
 })
